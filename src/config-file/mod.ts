0a5a95cb92d3f3bb2ef3466d98d268ff04927b06
Vicg853 <vicg853@gmail.com>
2022-01-24 19:03:43 -0300

    Fixing + adding get config module into the main configFile function

--- src/config-file/mod.ts
+++ src/config-file/mod.ts
@@ -1,6 +1,6 @@
 //* Assembles all config file functions in one
 import { ConfigOptions } from "./config-types.d.ts";
-import { checkExists } from "./file-checks.ts"
+import { getConfigsFromFile } from "./get-configs.ts"
 import { getConfigFilePath } from "./get.ts"
 
 interface ConfigFileMainOverload {
@@ -19,11 +19,22 @@ export const configFile: ConfigFileMainOverload = (args: string []) => {
    const configPath = configPathGet.configPath as string
 
    //* Checking if config file exists
-   if(!checkExists(configPath)) return {
+   if(!Deno.statSync(configPath).isFile) return {
       err: true,
       err_msg: configPathGet.err_msg,
       config: `The used '${configPath}' doesn't exist!`
    }
 
-   //TODO Get actual config file
+   //* Getting configs
+   const configs = getConfigsFromFile(configPath)
+   if(configs.err) return {
+      err: true,
+      err_msg: configs.err_msg,  
+      config: undefined
+   }
+
+   //*Checking if configs are valid
+
 }
+
+export const getScripts 
