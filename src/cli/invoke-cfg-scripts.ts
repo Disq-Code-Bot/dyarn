cf897aac0ade4dfb98026395c0d70575e333c16b
Vicg853 <vicg853@gmail.com>
2022-01-28 10:44:29 -0300

    Added invoker for config file scripts

---
+++ src/cli/invoke-cfg-scripts.ts
@@ -0,0 +1,56 @@
+import { configFile, getScripts } from '../config-file/mod.ts'
+import type { RunOptions } from '../../types/deno-types.d.ts'
+import { defaultFile } from '../global-defs.ts'
+
+interface InvokeCfgScriptsOverload {
+   (script: string, args: string[]): { success: boolean, cmdData?: RunOptions, hasScripts?: boolean, err_msg?: string }
+}
+
+//* Get config file and check for config scripts
+export const invokeCfgScripts: InvokeCfgScriptsOverload = 
+   (script: string, args: string[]) => {
+   //* Getting config 
+   const configs = configFile(args)
+
+   if(configs.err) return {
+      success: false,
+      err_msg: configs.err_msg as string
+   }
+
+   //* Getting scripts from config file
+   const scripts = getScripts(configs.config!)
+   
+   if(!scripts.hasScripts) return { 
+      success: false,
+      hasScripts: false 
+   }
+
+   //* Checking if script exists
+   if(!scripts.scripts[script]) return {
+      success: false,
+      err_msg: `Script "${script}" was not found in config file`
+   }
+
+   //* Getting script data
+   const scriptData = scripts.scripts[script]
+
+   //* Getting script runtime configs: file, flags, custom file, app flags 
+   const runFile = scriptData.customFile ?? configs.config?.mainFile ?? defaultFile
+   const runFlags = scriptData.flags?.split(' ') ?? []
+   const appFlags = scriptData.appFlags?.split(' ') ?? []
+   const runApp = ['deno', scriptData.invoker]
+   //TODO Add env vars
+
+   return {
+      success: true,
+      cmdData: {
+         cmd: runApp.concat(runFlags, runFile, appFlags),
+         //TODO add configurations to other Deno.run options
+         stdout: 'piped',
+         stderr: 'piped',
+         stdin: 'inherit'
+      },
+      hasScripts: true,
+   }
+
+}
