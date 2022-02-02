a62d037e9adc6f02ebafb3dc1a1f6c8a7fb1e923
Vicg853 <vicg853@gmail.com>
2022-02-01 22:21:15 -0300

    Small adaptation to later add dyarn built in commands

--- src/cli/mod.ts
+++ src/cli/mod.ts
@@ -1,7 +1,7 @@
 //* Iterates though all dyarn/deno/config file commands
 //* and calls the respective invoker
-import type { RunOptions } from '../../types/deno-types.d.ts'
 
+import { invokeDenoCommands } from './invoke-dyarn-cmd.ts'
 import { invokeCfgScripts } from './invoke-cfg-scripts.ts'
 
 
@@ -9,30 +9,26 @@ import { invokeCfgScripts } from './invoke-cfg-scripts.ts'
 export async function cli(script: string, args: string[]): 
    Promise<{ success: true } | { success: false, err: string }>
    {
-   let cmdOptions: RunOptions
+   
+   //* Dyarn own commands
    //TODO Add dyarn default commands
    //TODO Add other commands to be checked before looking for config file
 
    //*Checking for config file commands 
-   //TODO Uncomment this after adding Dyarn built in commands
-   //if(!cmdOptions) {
-      const cfgCmd = await invokeCfgScripts(script, args.slice(1))
+   const cfgCmd = await invokeCfgScripts(script, args.slice(1))
       
-      if(!cfgCmd.success && cfgCmd.hasScripts === false) return {
-         success: false,
-         err: `Neither any Dyarn built in commands nor any config file scripts with name "${args[0]}" were found!`
-      }
-
-      if(!cfgCmd.success) return {
-         success: false,
-         err: cfgCmd.err_msg!
-      }
-
-      cmdOptions = cfgCmd.cmdData!
-   //}
+   if(!cfgCmd.success && cfgCmd.hasScripts === false) return {
+      success: false,
+      err: `Neither any Dyarn built in commands nor any config file scripts with name "${args[0]}" were found!`
+   }
+   if(!cfgCmd.success) return {
+      success: false,
+      err: cfgCmd.err_msg!
+   }
 
    //* In case of success, run the command
-   const process = Deno.run(cmdOptions)
+   //TODO See about using Google's zx
+   const process = Deno.run(cfgCmd.cmdData!)
    
    const { success, code, signal } = await process.status()
 
