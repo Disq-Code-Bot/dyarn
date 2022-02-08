17cdc8988405c2809313795078dc15fb4477b3d4
Vicg853 <vicg853@gmail.com>
2022-02-08 12:11:20 -0300

    Improved dyarn built-in commands flags check

--- src/cli/invoke-dyarn-cmd.ts
+++ src/cli/invoke-dyarn-cmd.ts
@@ -2,32 +2,44 @@ import type { CommandFlags } from '../dyarn-internal/mod.ts'
 import { commands } from '../dyarn-internal/mod.ts'
 
 function checkFlags(args: string[], flagsToCheck: CommandFlags): { success: boolean, err_msg?: string } {
+   //* Getting flags and removing cli agrs characters
    const flags = args.filter(arg => arg.startsWith('--'))
       .map(arg => arg.replace('--', '').replace('=', ''))
    
-   if(flagsToCheck.required && !flags) return {
+   //* Checking if flags are required
+   if(flagsToCheck.required && (!flags || flags.length < 1)) return {
       success: false,
-      err_msg: `Any of the needed flags for this commands where provided!`
+      err_msg: `This command requires flags, use help command to discover more!`
    }
 
-   const missingFlags = flagsToCheck.arr.map(flagObj => {
-      if(!flagObj.required) return false
+   const filteredMissingFlags: string[] = []
+   flagsToCheck.arr.map(flagObj => {
+      if(!flagObj.required && !flags.includes(flagObj.flag)) return
       if(!flags.includes(flagObj.flag)) return flagObj.flag
 
       if(flagObj.dependsOnFlag){
-         const missingFlagDeps = 
-            flagObj.dependsOnFlag.every(depFlag => flags.includes(depFlag))
-         if(!missingFlagDeps) return flagObj.dependsOnFlag
+         const missingFlagDeps = flagObj.dependsOnFlag.filter(flag => !flags.includes(flag))
+         if(missingFlagDeps) return missingFlagDeps
       }
-      return false
-   })
+      return
+   }).filter(flag => !!flag).forEach(missingFlag => Array.isArray(missingFlag) ? 
+      missingFlag.forEach(subMissingFlag => 
+         !filteredMissingFlags.includes(subMissingFlag) && 
+            filteredMissingFlags.push(subMissingFlag)) 
+      : !filteredMissingFlags.includes(missingFlag!) && filteredMissingFlags.push(missingFlag!))
 
-   if(!missingFlags) return {
+   if(!filteredMissingFlags || !filteredMissingFlags[0]) return {
       success: true
    } 
    else return {
       success: false,
-      err_msg: `The following flags: ${missingFlags.join(', ')} are missing for this command!`
+      err_msg: `The following flag${
+         filteredMissingFlags.length > 1 ? 's' : ''
+      }: "${
+         filteredMissingFlags.join('", "')
+      }"; ${
+         filteredMissingFlags.length > 1 ? 'are' : 'is'
+      } missing for this command!`
    }
 }
 
